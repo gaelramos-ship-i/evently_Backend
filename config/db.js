78ee89bac39cbef7cda1bec2e39592672125e358
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-08-28 09:54:15 +0200

    change sequelize config for render

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URI)
+const sequelize = new Sequelize(process.env.DATABASE_URI, {
+  dialect: 'postgres',
+  dialectOptions: {
+    // Force IPv4 socket connection
+    family: 4, 
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Adjust according to your SSL requirements
+    }
+  }
+})
 
 const connectDB = async () => {
     try {
