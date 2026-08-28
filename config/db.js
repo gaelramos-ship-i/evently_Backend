a396b145219872bf472115833cbf19a5fae07dd2
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-08-28 12:15:37 +0200

    delete puppeteer and feat API openagenda

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URI, {
-  dialect: 'postgres',
-  dialectOptions: {
-    // Force IPv4 socket connection
-    family: 4, 
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Adjust according to your SSL requirements
-    }
-  }
-})
+const sequelize = new Sequelize(process.env.DATABASE_URI)
+
+// const sequelize = new Sequelize(process.env.DATABASE_URI, {
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     // Force IPv4 socket connection
+//     family: 4, 
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false // Adjust according to your SSL requirements
+//     }
+//   }
+// })
 
 const connectDB = async () => {
     try {
--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,59 +1,74 @@
 const { QueryTypes } = require("sequelize")
 const { sequelize } = require("../config/db")
 
-const puppeteer = require("puppeteer");
-
 /* US3: En tant qu'utilisateur, je veux rechercher un événement par mot-clé ou par lieu, afin de trouver rapidement une sortie qui m'intéresse. */
 
 exports.getEvent = async (req, res) => {
     try {
-        const { location } = req.query
-        if (!location) {
-            return res.status(400).json({ message: "veuillez renseigner une ville" })
+        const API_KEY_OPENAGENDA = process.env.API_KEY_OPENAGENDA
+        const { keyword, location } = req.query
+
+        if (!keyword && !location) {
+            return res.status(400).json({
+                message: "Veuillez renseigner un mot-clé ou une ville"
+            })
         }
 
-        const browser = await puppeteer.launch({
-            headless: false,
-        });
+        const params = new URLSearchParams()
 
-        const page = await browser.newPage();
-
-        const url = `https://www.ticketmaster.fr/fr/${location}`;
-
-        // on considère la page "chargée" quand il y a 2 requêtes réseau actives ou moins pendant au moins 500ms
-        await page.goto(url, { waitUntil: "networkidle2", timeout: 30000 });
-
-        // Attente explicite du contenu dynamique
-        const found = await page.waitForSelector('.event-result-title', { timeout: 10000 })
-            .then(() => true)
-            .catch(() => false);
-
-        if (!found) {
-            await browser.close();
-            return res.status(404).json({ message: "Aucun événement trouvé pour cette ville." });
+        if (keyword) {
+            params.append("search", keyword)
         }
 
-        const results = await page.evaluate(() => {
-            const articles = Array.from(document.querySelectorAll('article.event-result'));
-            return articles.map(article => ({
-                title: article.querySelector('.event-result-title-link')?.textContent.trim(),
-                date: article.querySelector('.event-result-date')?.textContent.trim(),
-                price: article.querySelector('.event-result-pricing')?.textContent.trim(),
-                place: article.querySelector('.event-result-place')?.textContent.trim(),
-                genre: article.querySelector('.event-result-genre-item')?.textContent.trim()
-            }));
-        });
+        if (location) {
+            params.append("location", location)
+        }
 
-        await browser.close();
+        const response = await fetch(
+            `https://api.openagenda.com/v2/agendas?${params.toString()}`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${API_KEY_OPENAGENDA}`,
+                }
+            }
+        )
 
-        res.json({
-            message: "Recherche réussie",
-            results
-        });
+        const data = await response.json()
+
+        if (!response.ok) {
+            return res.status(response.status).json(data)
+        }
+
+        const agendaUIDs = data.agendas.map(agenda => agenda.uid)
+
+        const agendas = await Promise.all(
+            agendaUIDs.map(async (uid) => {
+                const response = await fetch(
+                    `https://api.openagenda.com/v2/agendas/${uid}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${API_KEY_OPENAGENDA}`,
+                        }
+                    }
+                )
+
+                if (!response.ok) {
+                    return null
+                }
+
+                return await response.json()
+            })
+        )
+
+        return res.status(200).json({
+            agendas: agendas.filter(Boolean)
+        })
 
     } catch (err) {
-        res.status(500).json({
+        return res.status(500).json({
             message: "Erreur lors de la recherche des événements."
-        });
+        })
     }
-};
+}
