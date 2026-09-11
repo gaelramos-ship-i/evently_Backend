08e3d691a5852f89594442db1fc5f39152ec1d5e
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-09-11 14:26:52 +0200

    delete openagenda and feat api datatourism

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,66 +5,38 @@ const { sequelize } = require("../config/db")
 
 exports.getEvent = async (req, res) => {
     try {
-        const API_KEY_OPENAGENDA = process.env.API_KEY_OPENAGENDA
-        const { keyword, location } = req.query
+        const API_KEY_DATATOURISME = process.env.API_KEY_DATATOURISME;
+        const { keyword, department } = req.query;
 
-        if (!keyword && !location) {
+        if (!keyword && !department) {
             return res.status(400).json({
                 message: "Veuillez renseigner un mot-clé ou une ville"
-            })
+            });
         }
 
-        const search = keyword || location
+        const url = `https://api.datatourisme.fr/v1/entertainmentAndEvent?search=${keyword.toString()}&lang=fr&department=${department.toString()}`;
 
-        const response = await fetch(
-            `https://api.openagenda.com/v2/agendas?search=${search}`,
-            {
-                method: "GET",
-                headers: {
-                    key: API_KEY_OPENAGENDA,
-                }
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "X-API-Key": API_KEY_DATATOURISME,
             }
-        )
+        });
 
-        const data = await response.json()
+        const data = await response.json();
 
         if (!response.ok) {
-            return res.status(response.status).json(data)
+            return res.status(response.status).json(data);
         }
 
-        const agendaUIDs = data.agendas.map(agenda => agenda.uid)
-
-        const results = await Promise.all(
-            agendaUIDs.map(async (uid) => {
-                const response = await fetch(
-                    `https://api.openagenda.com/v2/agendas/${uid}/events?search=${search}`,
-                    {
-                        method: "GET",
-                        headers: {
-                            key: API_KEY_OPENAGENDA,
-                        }
-                    }
-                )
-
-                if (!response.ok) {
-                    return []
-                }
-
-                return await response.json()
-            })
-        )
-        const events = results.flatMap(result => result.events || [])
-
-        return res.status(200).json({
-            events
-        })
+        return res.status(200).json({ data });
 
     } catch (err) {
         return res.status(500).json({
             message: "Erreur lors de la recherche des événements."
-        })
+        });
     }
-}
+};
 
 exports.getDetails = async (req, res) => {
     try {
