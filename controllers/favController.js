ab09da2b32c020fd9972842c14df3ad04b672a1b
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-09-14 10:22:57 +0200

    update getFav logic for new DATAtourisme API payload

--- controllers/favController.js
+++ controllers/favController.js
@@ -55,18 +55,18 @@ exports.addFav = async (req, res) => {
 
 exports.getFav = async (req, res) => {
     try {
-
         const userId = req.user.id_user;
+        const API_KEY_DATATOURISME = process.env.API_KEY_DATATOURISME;
 
         const favoris = await sequelize.query(` 
-            SELECT uid_agenda, uid_event, date_ajout 
+            SELECT uuid_event, date_ajout 
             FROM "Favoris" 
             WHERE fk_id_user = :id_user 
             ORDER BY date_ajout DESC
         `, {
             replacements: { id_user: userId },
             type: QueryTypes.SELECT
-        })
+        });
 
         async function findCategory(name) {
             const existing = await sequelize.query(`
@@ -74,101 +74,104 @@ exports.getFav = async (req, res) => {
             `, {
                 replacements: { name },
                 type: QueryTypes.SELECT
-            })
+            });
 
             if (existing.length > 0) {
-                return existing[0].id_category
+                return existing[0].id_category;
             }
 
             const created = await sequelize.query(`
-                INSERT INTO "Categories" (name_category) VALUES (:name)
+                INSERT INTO "Categories" (name_category) VALUES (:name) RETURNING id_category
             `, {
                 replacements: { name },
                 type: QueryTypes.INSERT
-            })
+            });
 
-            return created[0][0].id_category
+            return created[0][0].id_category;
         }
 
         const events = await Promise.all(
-        favoris.map(async (fav) => {
-            const response = await fetch(
-                `https://api.openagenda.com/v2/agendas/${fav.uid_agenda}/events/${fav.uid_event}`,
-                {
-                    method: "GET",
-                    headers: {
-                        key: process.env.API_KEY_OPENAGENDA,
+            favoris.map(async (fav) => {
+                const response = await fetch(
+                    `https://api.datatourisme.fr/v1/catalog/${fav.uuid_event}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "X-API-Key": API_KEY_DATATOURISME,
+                        }
                     }
+                );
+
+                if (!response.ok) {
+                    return null;
                 }
-            )
 
-            if (!response.ok) {
-                return null
-            }
+                const data = await response.json();
 
-            const data = await response.json()
-            const event = data.event
-
-            const eventExist = await sequelize.query(`
+                const eventExist = await sequelize.query(`
                     SELECT 1 FROM "Events" WHERE uid_event = :uidEvent
                 `, {
-                replacements: { uidEvent: fav.uid_event },
-                type: QueryTypes.SELECT
+                    replacements: { uidEvent: fav.uuid_event },
+                    type: QueryTypes.SELECT
+                });
+
+                if (eventExist.length === 0) {
+                    const title = data.label?.['@fr'] || data.label?.['@en'] || 'Titre non disponible';
+                    const shortDesc = data.hasDescription?.[0]?.shortDescription?.['@fr'] || null;
+                    const desc = data.comment?.['@fr'] || null;
+                    
+                    const location = data.isLocatedAt?.[0];
+                    const addressObj = location?.address?.[0];
+                    const city = addressObj?.hasAddressCity?.label?.['@fr'] || addressObj?.addressLocality || null;
+                    const address = addressObj?.streetAddress ? addressObj.streetAddress.join(', ') : null;
+
+                    const imageUrl = data.hasMainRepresentation?.[0]?.hasObservation?.[0]?.hasUrl || null;
+                    const sourceUrl = data.hasContact?.[0]?.homepage?.[0] || data.uri || null;
+                    const dateEvent = data.takesPlaceAt?.[0]?.startDate || null;
+                    const priceEvent = null;
+
+                    const idCategory = await findCategory('Autre');
+
+                    await sequelize.query(` 
+                        INSERT INTO "Events" (uid_event, title_event, shortdesc_event, desc_event, date_event, img_url, place_event, city_event, source_url, price_event, fk_id_category)
+                        VALUES (:uidEvent, :title, :shortDesc, :desc, :dateEvent, :imgUrl, :placeEvent, :city, :sourceUrl, :priceEvent, :idCategory)
+                    `, {
+                        replacements: {
+                            uidEvent: fav.uuid_event,
+                            title,
+                            shortDesc,
+                            desc,
+                            dateEvent,
+                            imgUrl: imageUrl,
+                            placeEvent: address,
+                            city,
+                            sourceUrl,
+                            priceEvent,
+                            idCategory
+                        },
+                        type: QueryTypes.INSERT
+                    });
+                }
+
+                return {
+                    ...data,
+                    date_ajout: fav.date_ajout
+                };
             })
+        );
 
-            if (eventExist.length === 0) {
-                const title = event.title.fr
-                const shortDesc = event.description.fr
-                const desc = event.longDescription.fr
-                const city = event.location.city
-                const address = event.location.address
-                const filename = event.image.filename
-                const imageUrl = `${event.image.base}${filename}`
-                const sourceUrl = event.originAgenda.url
-                const dateEvent = event.nextTiming.begin
-                const priceEvent = event.conditions.fr
-                const idCategory = await findCategory('Autre')
+        const validEvents = events.filter(Boolean);
 
-                await sequelize.query(` 
-                    INSERT INTO "Events" (uid_event, title_event, shortdesc_event, desc_event, date_event, img_url, place_event, city_event, source_url, price_event, fk_id_category)
-                    VALUES (:uidEvent, :title, :shortDesc, :desc, :dateEvent, :imgUrl, :placeEvent, :city, :sourceUrl, :priceEvent, :idCategory)
-                `, {
-                    replacements: {
-                        uidEvent: fav.uid_event,
-                        title,
-                        shortDesc,
-                        desc,
-                        dateEvent,
-                        imgUrl: imageUrl,
-                        placeEvent: address,
-                        city,
-                        sourceUrl,
-                        priceEvent,
-                        idCategory
-                    },
-                    type: QueryTypes.INSERT
-                })
-            }
+        return res.status(200).json({
+            favoris: validEvents
+        });
 
-            return {
-                ...event,
-                date_ajout: fav.date_ajout
-            }
-        })
-    )
-
-    const validEvents = events.filter(Boolean)
-
-    return res.status(200).json({
-        favoris: validEvents
-    })
-
-} catch (err) {
-    return res.status(500).json({
-        message: "Erreur lors de l'affichage des favoris"
-    })
-}
-}
+    } catch (err) {
+        return res.status(500).json({
+            message: "Erreur lors de l'affichage des favoris"
+        });
+    }
+};
 
 exports.deleteFav = async (req, res) => {
     try {
