5a5615f5fe47ac2019b6f5b7bc3c1394fd8df933
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-09-15 10:58:19 +0200

    update: tests favoris

--- test/fav.test.js
+++ test/fav.test.js
@@ -1,10 +1,11 @@
-const { test, describe, before, after } = require('node:test')
+const { test, describe, before, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 require('dotenv').config()
 
 const { sequelize } = require('../config/db')
 
 const { addFav, getFav, deleteFav } = require('../controllers/favController')
+const { register } = require('../controllers/authController')
 
 const createMockRes = () => {
     const res = {
@@ -24,21 +25,37 @@ const createMockRes = () => {
 
 describe("US4: addFav", () => {
     const fakeUuidEvent = "123e4567-e89b-12d3-a456-426614174000"
+    const testEmail = "test_us_a@example.com"
+    let userA = null
+
+    beforeEach(async () => {
+        const reqA = {
+            body: {
+                name: 'Alice Tester',
+                email: 'test_us_a@example.com',
+                password: 'Password123!'
+            }
+        }
+
+        const resA = createMockRes()
+        await register(reqA, resA)
+
+        userA = resA.body.user
+    })
 
     after(async () => {
-        const userId = userA.id_user
-        if (userId) {
+        if (userA) {
             await sequelize.query(
-                'DELETE FROM "Favoris" WHERE fk_id_user = :userId',
-                { replacements: { userId } }
+                'DELETE FROM "Users" WHERE email_user = :email',
+                { replacements: { email: testEmail } }
             )
         }
+        await sequelize.close()
     })
 
     test("Doit renvoyer 400 si uuidEvent n'est pas fourni dans les params", async () => {
-        const userId = userA.id_user
         const req = {
-            user: { id_user: userId },
+            user: { id_user: await userA.id_user },
             params: {}
         }
         const res = createMockRes()
@@ -50,9 +67,8 @@ describe("US4: addFav", () => {
     })
 
     test("Doit renvoyer 201, et ajouter l'événement au favori", async () => {
-        const userId = userA.id_user
         const req = {
-            user: { id_user: userId },
+            user: { id_user: userA.id_user },
             params: { uuidEvent: fakeUuidEvent }
         }
         const res = createMockRes()
